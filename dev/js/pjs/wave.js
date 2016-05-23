be214ed2f60ebc87b68180bbcc622d0680d29ada
Barrior <barrior@qq.com>
2016-05-23 19:50:08 +0800

    wave

--- dev/js/pjs/wave.js
+++ dev/js/pjs/wave.js
@@ -4,6 +4,8 @@
 
     var util = Particleground.util,
         random = Math.random,
+        sin = Math.sin,
+        radian = Math.PI / 180,
         pi2 = Math.PI * 2;
 
     function Wave( selector, options ){
@@ -21,45 +23,112 @@
     Wave.configDefault = {
         //全局透明度
         opacity: 1,
-        //雪花颜色
-        color: '#fff',
-        //雪花最大半径
-        max: 6.5,
-        //雪花最小半径
-        min: .4,
+        //线条颜色
+        color: [util.randomColor(), util.randomColor(), util.randomColor()],
+        //线条个数
+        num: 3,
+        //线条宽度
+        lineWidth: 1,
+        //波峰数值,容器一半高*此数值
+        crest: 1,
         //运动速度
-        speed: .4,
+        speed: 6,
         //自适应窗口尺寸变化
         resize: true
     };
 
+
     Wave.prototype = {
         version: '1.0.0',
         createDot: function(){
+            var set = this.set,
+                ch = this.ch,
+                crest = this.ch / 2 * set.crest,
+                num = set.num,
+                dotsNum = this.cw / 2,
+                dots = [],
 
-        },
-        draw: function(){
-            var self = this,
-                set = self.set,
-                cxt = self.cxt,
-                cw = self.cw,
-                ch = self.ch,
-                dots = self.dots;
+                calc = ( set.max - set.min ) / dotsNum;
 
-            cxt.clearRect( 0, 0, cw, ch );
+            for( var i = 0; i < num; i++ ){
 
-            //当canvas宽高改变的时候，全局属性需要重新设置
-            if( set.resize ){
-                cxt.fillStyle = set.color;
-                cxt.globalAlpha = set.opacity;
+                var scale = 1 - i / set.num;
+                var	arr = [];
+
+                for( var j = 0; j < dotsNum; j++ ){
+                    arr.push({
+                        x: j * 2,
+                        y: j / dotsNum * crest * scale,
+                        //y: ch / ( 30 - 27.6 * j / dotNum ),
+                        angle: j,
+                        //r: j * calc  + set.min
+                        r: 1
+                    });
+                }
+
+                dots.push( arr );
+
             }
 
-            cxt.strokeStyle ="#FF5D43";
+            this.dots = dots;
+        },
+        draw: function(){
+            var set = this.set,
+                cxt = this.cxt,
+                cw = this.cw,
+                ch = this.ch,
+                halfCH = ch / 2,
+                speed = set.speed;
+
+            cxt.clearRect( 0, 0, cw, ch );
+            cxt.globalAlpha = set.opacity;
+
+            /*cxt.save();
             cxt.beginPath();
-            cxt.moveTo(0,200);
-            cxt.quadraticCurveTo(150,100,300,200);
-            cxt.quadraticCurveTo(450,300,600,200);
+            cxt.moveTo(20, 200);
             cxt.stroke();
+            cxt.restore();
+            cxt.save();
+            cxt.beginPath();
+            cxt.lineTo(200, 200);
+            cxt.stroke();
+            cxt.restore();*/
+
+            this.dots.forEach(function( lineDots, i ){
+                var color = set.color[i];
+                cxt.save();
+                cxt.beginPath();
+                lineDots.forEach(function( v, j ){
+                    if( j ){
+                        //cxt.lineTo(200, 200)
+                        cxt.lineTo( v.x, v.y * sin( v.angle * radian ) + halfCH );
+                    }else{
+                        //cxt.moveTo(20, 200)
+                        cxt.moveTo( v.x, v.y * sin( v.angle * radian ) + halfCH );
+                    }
+                    v.angle -= speed;
+                });
+                cxt.strokeStyle = color;
+                cxt.stroke();
+                cxt.restore();
+            });
+            /*this.dots.forEach(function( lineDots, i ){
+                var color = set.color[i];
+                lineDots.forEach(function( v, j ){
+                    cxt.save();
+                    cxt.beginPath();
+                    cxt.arc(
+                        v.x,
+                        //y = A sin（ ωx + φ ）+ h
+                        v.y * sin( v.angle * radian ) + halfCH,
+                        v.r, 0, pi2
+                    );
+                    cxt.fillStyle = color;
+                    cxt.fill();
+                    cxt.restore();
+                    v.angle -= speed;
+                });
+            });*/
 
             //this.requestAnimationFrame();
         }
