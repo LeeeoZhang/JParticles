8dce516f7a0835b3cd25cb41894c633c7df75120
barrior <barrior@qq.com>
2017-03-29 23:58:17 +0800

    save

--- gulpfile.dev.js
+++ gulpfile.dev.js
@@ -1,29 +1,43 @@
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
 
 const fs = require('fs');
-const path = './dev/';
+const devPath = './dev/';
+const destPath = './production/';
 
 // Pack all js to generating 'jparticles.all.js' at dev directory.
 gulp.task('pack', () => {
-    gulp.watch([path + '*.js'], () => {
-        fs.readdir(path, (err, files) => {
+    gulp.watch([devPath + '*.js'], () => {
+        fs.readdir(devPath, (err, files) => {
 
             files = files.join(' ').replace(/jparticles\.js\s|jparticles\.all\.js\s/g, '');
             files = ('jparticles.js ' + files).split(' ');
             files.forEach((item, i, array) => {
-                array.splice(i, 1, path + item);
+                array.splice(i, 1, devPath + item);
             });
 
             gulp.src(files)
                 .pipe(concat('jparticles.all.js'))
-                .pipe(gulp.dest(path))
+                .pipe(gulp.dest(devPath))
         });
     });
 });
 
-gulp.task('default', ['pack'], () => {
+gulp.task('compile', () => {
+    gulp.src(devPath + '*.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourcemaps.write('./maps/', {
+            includeContent: false,
+            sourceRoot: '../../dev'
+        }))
+        .pipe(gulp.dest(destPath));
+});
+
+gulp.task('default', ['pack', 'compile'], () => {
     // do something else if the pack task is successful...
 });
