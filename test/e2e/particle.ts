4993df3f32c825aedb6654a81e3628cc3708b489
barrior <barrior@qq.com>
2017-07-12 21:16:30 +0800

    adjust

--- test/e2e/particle.ts
+++ test/e2e/particle.ts
@@ -7,11 +7,21 @@ test(`Canvas width and height should equal to the container`, async t => {
     const demo = Selector('#instance1 .demo');
     const canvas = Selector('#instance1 .demo canvas');
 
-    const dw = await demo.getStyleProperty('width').then(value => value);
-    const dh = await demo.getStyleProperty('height').then(value => value);
-    const cw = await canvas.getStyleProperty('width').then(value => value);
-    const ch = await canvas.getStyleProperty('height').then(value => value);
+    function testWH() {
+        return new Promise(async resolve => {
+            const dw = await demo.getStyleProperty('width').then(value => value);
+            const dh = await demo.getStyleProperty('height').then(value => value);
+            const cw = await canvas.getStyleProperty('width').then(value => value);
+            const ch = await canvas.getStyleProperty('height').then(value => value);
 
-    await t.expect(parseInt(cw)).eql(parseInt(dw));
-    await t.expect(parseInt(ch)).eql(parseInt(dh));
+            await t.expect(parseInt(cw)).eql(parseInt(dw));
+            await t.expect(parseInt(ch)).eql(parseInt(dh));
+
+            resolve();
+        });
+    }
+
+    await testWH();
+    await t.resizeWindow(1000, 400);
+    await testWH();
 });
